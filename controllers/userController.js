9d52a793c334c60e4df44763a402ea14acb5e6a6
jaredhdev <jared.haby@gmail.com>
2024-08-14 17:33:43 +1000

    Add signup and login routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const User = require("../models/userModels")
 
 exports.signUp = async (req, res) => {
     try {
-        const { username, password} = req.body
+        const { username, password } = req.body
 
         if (!username || !password) {
             return res
@@ -32,8 +32,51 @@ exports.signUp = async (req, res) => {
         return res
             .status(201)
             .json({ message: "User created successfully", newUser });
+
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({ message: "Error creating user" })
     }
 }
+
+exports.login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+
+        if (!username || !password) {
+            return res
+                .status(400)
+                .json({message: "Please input username and password"})
+        }
+
+        const user = await User.findOne({username})
+
+        if (!user) {
+            return res.status(401).json({message: "Invalid username or password"})
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+
+        if (!passwordMatch) {
+            return res.status(401).json({message: "Invalid username or password"})
+        }
+
+        // Generate JWT token
+
+        const token = jwt.sign(
+            {userId: user._id, username: username},
+            process.env.SECRET_KEY || "1234!@#%<{*&}",
+            {expiresIn: "1h"}
+        )
+
+        return res
+            .status(200)
+            .json({message: "Login Successful", data: user, token})
+
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ message: "Error during login" })
+    }
+
+}
+
--- routes/user.Routes.js
+++ routes/user.Routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { signUp } = require("../controllers/userController");
+const { signUp, login } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post('/login', login);
 
 module.exports = router;
